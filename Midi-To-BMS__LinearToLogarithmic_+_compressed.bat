f59bf14421b4c2a691115c95e8263667c625666a
Louiskovski <38958474+Louiskovski@users.noreply.github.com>
2026-01-03 21:06:25 +0100

    Update Midi-To-BMS__LinearToLogarithmic_+_compressed.bat

--- Midi-To-BMS__LinearToLogarithmic_+_compressed.bat
+++ Midi-To-BMS__LinearToLogarithmic_+_compressed.bat
@@ -1,8 +1,9 @@
 @echo off
 set Yaz0EncPath="C:\Tools\yaz0enc.exe"
 
-python %~dp0MIDI-to-BMS.py "%~1" "%~1.bms" True
+python %~dp0MIDI-to-BMS.py "%~1" "%~1.bms" True False
 
 %Yaz0EncPath% "%~1.bms"
 
 pause
+
